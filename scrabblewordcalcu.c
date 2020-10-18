8ced683032bdee76ef3835c3dfd75cb1fb117f87
Alexander Knight <72681685+alexander-knight@users.noreply.github.com>
2020-10-18 21:29:13 +0800

    Update scrabblewordcalcu.c

--- scrabblewordcalcu.c
+++ scrabblewordcalcu.c
@@ -1,10 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Declare score counting function
+int compute_score(string word);
+
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-
-int compute_score(string word);
 
 int main(void)
 {
@@ -16,17 +17,31 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
-    // compa
+    // Print the winner
+    if(score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else if(score1 < score2)
+    {
+        printf("Player 2 wins!");
+    }
+    else
+    {
+        printf("Tie!");
+    }
 }
 
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    // 1. make all letters be upper case
-    // 2. get ascii equivalent integer
-    // 3. subtract 65, pass this to variable i
-    //      3.5 instantiate score data to variable "score"
-    // 4. get the number at array points[i] and add it to score
+    // .1 get len of array and assign it to var arraylen
+    // .2 make loop here, repeat while counter var i >= arraylen
+    // 1. make all letters be upper case - Run letter of index i to to upper
+    // 2. get ascii equivalent integer - cast arr[i] as int
+    // 3. subtract 65, pass this to variable i - subtract 65 from int arr[i]
+    //      3.5 instantiate score data to variable "score" = 0
+    // 4. get the number at arr[i] and add it to score
+    //      4.5 repeat until out of letters
     // 5. return score
 }
