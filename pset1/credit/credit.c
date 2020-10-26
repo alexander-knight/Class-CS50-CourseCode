19fd3d13cb5940c1427f474f135b92a5c1df662b
Alexander Knight <72681685+alexander-knight@users.noreply.github.com>
2020-10-27 06:30:52 +0800

    Update credit.c

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -2,19 +2,33 @@
 #include <cs50.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+// Prototyping card type identifier function
+int card_type(long cardnum);
+// And the number isolator function
+int number_isolator(int cardnum)
+
 
 int main(void)
 {
-    long long cardnum;
+    long cardnum;
     // We take the visa card number...
     do
     {
-        cardnum = get_long_long("Card Number: ");
+        cardnum = get_long("Card Number: ");
     }
     // And keep prompting until the number is between these 2 lengths
-    while ( cardnum < (10 ^ 13) || cardnum > (10 ^ 19) );
-    
-    printf("%ll\n", cardnum);
+    while (    cardnum < (pow (10, 13))
+            || cardnum > (pow (10, 19))
+          );
+
+    printf("%l\n", cardnum);
+
+
+
+
+        
 }
 
 
@@ -22,16 +36,34 @@ int number_isolator(int cardnum)
 {
     // Initialise the variable init adder to hold the isolated number before adding it to the check sum
     int init_adder;
-    
+
     // Isolates the number at the 10 ^ (i+1) place
     for (int i = 0; i < 19; i++ )
-    init_adders = ( ( cardnum % (10 ^ (i+1)) - cardnum % (10 ^ i) ) / (10^i) );
-    // If-statement to sort out every other number
+    {
+        init_adder = (
+                   ( cardnum % (pow (10, (i+1)))
+                   - cardnum % (pow (10, (i)  ))
+                   )
+                   / (pow (10, i) )
+                 );
+
+
+        // If-statement to sort out every other number
         // To filter out every other number starting with the ten place
         if (i % 2 == 0)
         {
             init_adder *= 2;
         }
-    // Then add to the checksum
-    int checksum += init_adder
+        
+        // Then add to the checksum
+    int checksum += init_adder;
+    }
+
+
+}
+
+// Takes only a valid card and returns the card type (Amex, Mastercard or Visa)
+int card_type(long cardnum)
+{
+
 }
